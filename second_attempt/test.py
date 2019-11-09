8fb543c304c64478743b7a39269c731b75e885bd
Christian Parpart <christian@parpart.family>
2019-11-10 00:33:16 +0100

    snip

--- second_attempt/test.py
+++ second_attempt/test.py
@@ -31,13 +31,25 @@ def construct(d, n):
     """
     Constructs A(l, d) in R^{(n-1)^l x (n-1)^l}
     """
-    def generate(l):
+    def generate(l, global_i=1, global_j=1):
+        def construct_A1():
+            """ Constructs a A1 matrix in R^{(n-1)*(n-1)}. """
+            def row(i):
+                def entry(j):
+                    if i == j:
+                        return 2 * d
+                    elif j == i - 1 or j == i + 1:
+                        return -1
+                    else:
+                        return 0
+                return [ entry(j) for j in range(1, n) ]
+            return [ row(i) for i in range(1, n) ]
         def row(i):
             def entry(j):
                 def null(k):
                     return [ [0 for j in range(1, k + 1)] for i in range(1, k + 1)]
                 def minus_I(k):
-                    return [ [(-1 if i == j else 0) for j in range(1, k + 1)] for i in range(1, k + 1)]
+                    return [ [(-1 if i == j else 0) for j in range(1, k + 1)] for i in range(1, k + 1)] # XXX
                 if i == j:
                     return generate(l - 1)
                 elif j == i - 1 or j == i + 1:
@@ -48,12 +60,12 @@ def construct(d, n):
         if l > 1:
             return [ row(i) for i in range(1, n) ]
         elif l == 1:
-            return construct_A1(d, n)
+            return construct_A1()
         else:
             raise ValueError("Invalid parameter l={0}.".format(l))
     if not (n >= 2):
         raise ValueError("n must be >= 2")
-    return list(generate(d))
+    return generate(d, 1, 1)
 
 A_n = construct(2, 3)
 print(A_n)
