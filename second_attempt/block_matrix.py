3426111c51b06ab6514f6bf7e75fb31e9e12535e
Christian Parpart <christian@parpart.family>
2019-11-15 21:17:10 +0100

    final cleanup

--- second_attempt/block_matrix.py
+++ second_attempt/block_matrix.py
@@ -5,54 +5,34 @@ Date: 2019-11-13
 License: GPL-3
 """
 
-import functools as fp
 from scipy import sparse as sm
 
 def construct(d, n):
     """
-    Constructs block matrices arising from finite difference approximations
-    of the Laplace operator.
+        Constructs block matrices arising from finite difference approximations of the Laplace
+        operator.
 
-    Attributes
-    ----------
-    d : int
-        Dimension of the space
-    n : int
-        Number of intervals in each dimension
+        Parameters
+        ----------
+        d : int
+            Dimension of the space
+        n : int
+            Number of intervals in each dimension
 
-    Returns
-    -------
-    list
-        A row-major list of block matrix entries suitable for feeding into SciPy's API.
-    """
-    """
-        construct(d, n)
-            generate(l)
-                construct_A1()
-                    row(i)
-                        entry(j)
-                row(i)
-                    entry(j)
+        Returns
+        -------
+        scipy.sparse.coo_matrix
+            block_matrix in a sparse data format
     """
     def generate(l):
-        def construct_A1(): # pylint: disable=invalid-name
-            def row(i):
-                return [{
-                    0: lambda: 2 * d,
-                    1: lambda: -1
-                }.get(abs(i - j), lambda: 0)() for j in range(1, n)]
-            return sm.coo_matrix([row(i) for i in range(1, n)])
-        def row(i):
-            return [{
-                0: lambda: generate(l - 1),
-                1: lambda: -1 * sm.identity((n - 1) ** (l - 1))
-            }.get(abs(i - j), lambda: None)() for j in range(1, n)]
-        res = [row(i) for i in range(1, n)] if l > 1 else construct_A1()
+        res = [[{
+            0: lambda: generate(l - 1),
+            1: lambda: -1 * sm.identity((n - 1) ** (l - 1))
+        }.get(abs(i - j), lambda: None)() for j in range(1, n)] for i in range(1, n)] if l > 1 else sm.coo_matrix([[{
+            0: lambda: 2 * d,
+            1: lambda: -1
+        }.get(abs(i - j), lambda: 0)() for j in range(1, n)] for i in range(1, n)])
         return sm.bmat(res) if isinstance(res, list) and isinstance(res[0], list) else res
-    if not d >= 1:
-        raise ValueError("d must satisfy >= 1")
-    if not n >= 2:
-        raise ValueError("n must satisfy >= 2")
     return generate(d)
 
 class BlockMatrix:
@@ -76,7 +56,16 @@ class BlockMatrix:
             Dimension of the space
         n : int
             Number of intervals in each dimension
+
+        Raises
+        ------
+        ValueError, if d < 1 or n < 2
         """
+        if not d >= 1:
+            raise ValueError("d must satisfy >= 1")
+        if not n >= 2:
+            raise ValueError("n must satisfy >= 2")
+
         self.d = d
         self.n = n
         self.extend = (n - 1) ** d
@@ -117,10 +106,19 @@ class BlockMatrix:
 
         return non_zeros, zeros, rel_non_zeros, rel_zeros
 
-def test_main():
-    """ internal demo-function for testing this. """
+def main():
+    """ Internal demo-function for testing (or demoing) this module. """
     def demo(d, n):
-        """ Demos block matrix for given `d` and `n` onto the terminal. """
+        """
+        Demos block matrix for given dimension `d` and number of grid points `n` onto the terminal.
+
+        Parameters:
+        -----------
+        d : int
+            Dimension of the space
+        n : int
+            Number of intervals in each dimension
+        """
         mat = BlockMatrix(d, n)
         print("# Demo BlockMatrix(d={}, n={}) in R^{{{}x{}}}".format(d, n, mat.extend, mat.extend))
         print("#==============================================")
@@ -134,9 +132,9 @@ def test_main():
         print("% non-zeros : {:.2} (relative non-zero values)".format(rel_non_zeros))
         print("% zeros     : {:.2} (relative zero values)".format(rel_zeros))
         print()
-    for d in [1, 2, 3, 4]:
+    for d in [1, 2, 3]:
         for n in [2, 3, 4, 5]:
             demo(d, n)
 
 if __name__ == "__main__":
-    test_main()
+    main()
