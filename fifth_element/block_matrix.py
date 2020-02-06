be8796b3ddce9ec6ba668d6e742351d8aabd8c46
keithoma <thomakmj@gmail.com>
2020-02-06 22:26:06 +0100

    The solution is still wrong
    but at least its running

---
+++ fifth_element/block_matrix.py
@@ -0,0 +1,354 @@
+#! /usr/bin/env python3
+"""
+This module implements the BlockMatrix class which is used to solve the Poisson problem.
+
+Author: Christian Parpart & Kei Thoma
+Date: 2019-11-13
+License: GPL-3
+"""
+
+import numpy as np
+from scipy import sparse as sm
+from scipy.sparse import linalg as slina
+
+import matplotlib
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt # pylint: disable=wrong-import-position
+
+matplotlib.rc('xtick', labelsize=20)
+matplotlib.rc('ytick', labelsize=20)
+
+def construct(d, n):
+    """
+        Constructs block matrices arising from finite difference approximations of the Laplace
+        operator.
+
+        Parameters
+        ----------
+        d : int
+            Dimension of the space
+        n : int
+            Number of intervals in each dimension
+
+        Returns
+        -------
+        scipy.sparse.coo_matrix
+            block_matrix in a sparse data format
+    """
+    def generate(l):
+        """ Auxilary function.
+
+        Parameters
+        ----------
+        l : int
+            The current index of the recursion.
+
+        Returns
+        -------
+        sparse matrix
+            The block matrix depending on l.
+        """
+        res = [[{
+            0: lambda: generate(l - 1),
+            1: lambda: -1 * sm.identity((n - 1) ** (l - 1))
+        }.get(abs(i - j), lambda: None)() for j in range(1, n)] for i in range(1, n)] if l > 1 else sm.coo_matrix([[{
+            0: lambda: 2 * d,
+            1: lambda: -1
+        }.get(abs(i - j), lambda: 0)() for j in range(1, n)] for i in range(1, n)])
+        return sm.bmat(res) if isinstance(res, list) and isinstance(res[0], list) else res
+    return generate(d)
+
+class BlockMatrix:
+    """ Represents block matrices arising from finite difference approximations
+    of the Laplace operator.
+
+
+    Attributes
+    ----------
+    d : int
+        Dimension of the space.
+    n : int
+        Number of intervals in each dimension.
+    extend : int
+        The size of the matrix on one axis.
+    data : sparse
+        The matrix saved as SciPy sparse format. The format is appropriately chosen by SciPy.
+    """
+
+    def __init__(self, d, n):
+        """
+        Parameters
+        ----------
+        d : int
+            Dimension of the space
+        n : int
+            Number of intervals in each dimension
+
+        Raises
+        ------
+        ValueError, if d < 1 or n < 2
+        """
+        if not d >= 1:
+            raise ValueError("d must satisfy >= 1.")
+        if not n >= 2:
+            raise ValueError("n must satisfy >= 2.")
+
+        self.d = d
+        self.n = n
+        self.extend = (n - 1) ** d
+        self.data = construct(d, n)
+
+    def get_sparse(self):
+        """ Returns the block matrix as sparse matrix.
+
+        Returns
+        -------
+        scipy.sparse.csr_matrix
+            block_matrix in a sparse data format
+        """
+        return self.data.tocsr()
+
+    def eval_zeros(self):
+        """ Returns the (absolute and relative) numbers of (non-)zero elements
+        of the matrix. The relative number of the (non-)zero elements are with
+        respect to the total number of elements of the matrix.
+
+        Returns
+        -------
+        int
+            number of non-zeros
+        int
+            number of zeros
+        float
+            relative number of non-zeros
+        float
+            relative number of zeros
+        """
+        sp = self.get_sparse()
+        total_elems = self.extend ** 2
+        non_zeros = sp.nnz
+        zeros = total_elems - non_zeros
+        rel_non_zeros = non_zeros / total_elems
+        rel_zeros = zeros / total_elems
+
+        return non_zeros, zeros, rel_non_zeros, rel_zeros
+
+    # ----- ----- ----- -----
+
+    def get_lu(self):
+        """ Provides an LU-Decomposition of the represented matrix A of the
+        form pr * A * pc = l * u
+
+        Returns
+        -------
+        pr : scipy.sparse.csr_matrix
+            row permutation matrix of LU-decomposition
+        l : scipy.sparse.csr_matrix
+            lower triangular unit diagonal matrix of LU-decomposition
+        u : scipy.sparse.csr_matrix
+            upper triangular matrix of LU-decomposition
+        pc : scipy.sparse.csr_matrix
+            column permutation matrix of LU-decomposition
+        """
+        lu = slina.splu(sm.csc_matrix(self.data))
+
+        # convert the triangular matrix to csr (from csc)
+        l, u = lu.L.tocsr(), lu.U.tocsr()
+
+        # reconstruct and convert the permutation matrix to csr
+        pr, pc = np.zeros((self.extend, self.extend)), np.zeros((self.extend, self.extend))
+        pr[lu.perm_r, np.arange(self.extend)], pc[np.arange(self.extend), lu.perm_c] = 1, 1
+        pr, pc = sm.csr_matrix(pr), sm.csr_matrix(pc)
+
+        return pr, l, u, pc
+
+    def eval_zeros_lu(self):
+        """ Returns the absolute and relative numbers of (non-)zero elements of
+        the LU-Decomposition. The relative quantities are with respect to the
+        total number of elements of the represented matrix.
+
+        Returns
+        -------
+        int
+            Number of non-zeros
+        int
+            Number of zeros
+        float
+            Relative number of non-zeros
+        float
+            Relative number of zeros
+        """
+        _, l, u, _ = self.get_lu()
+
+        # absolute number of zeros for LU
+        nnz = l.count_nonzero() + u.count_nonzero() - self.extend
+        nz = self.extend ** 2 - nnz
+
+        # absolute number of zeros for A
+        mat_nnz = self.data.count_nonzero()
+        mat_nz = self.extend ** 2 - mat_nnz
+
+        return nnz, nz, float(mat_nnz / nnz) if nnz != 0 else 1, float(mat_nz / nz) if nz != 0 else 1
+
+    def get_cond(self):
+        """ Computes the condition number of the represented matrix.
+
+        Returns
+        -------
+        float
+            condition number with respect to max-norm
+        """
+        # if n is 2, then the matrix has the dimension 1x1
+        if self.n == 2:
+            return 1
+
+        # SciPy likes csc more to calculate the inverse
+        csc = sm.csc_matrix(self.data)
+        return sm.linalg.norm(csc, np.inf) * sm.linalg.norm(sm.linalg.inv(csc), np.inf)
+
+def draw_cond(max_n=15):
+    """
+    Draws the graph for the condition of A.
+
+    Parameters
+    ----------
+    max_n : int
+        The maximal n for which the plot should be drawn.
+    """
+    # get the values for the plot
+    for d in [1, 2, 3]:
+        plt.loglog([x for x in range(2, max_n)],
+                   [BlockMatrix(d, x).get_cond() for x in range(2, max_n)],
+                   label="cond" + "$(A_" + str(d) + ")$", linewidth=3)
+
+    # finilize
+    plt.xlabel('$N$', fontsize=18)
+    plt.ylabel('$cond(A)$', fontsize=18)
+    plt.legend(fontsize=24)
+    plt.title("Plot of cond(A) depending on the dimension and size", fontsize=24)
+    plt.show()
+
+def draw_nonzero(max_n=15):
+    """
+    Draws the graph for the nonzero entries of A.
+
+    Parameters
+    ----------
+    max_n : int
+        The maximal n for which the plot should be drawn.
+    """
+    # get the values for the plot
+    for d in [1, 2, 3]:
+        plt.loglog([x for x in range(2, max_n)],
+                   [BlockMatrix(d, x).eval_zeros()[0] for x in range(2, max_n)],
+                   label="nonzero elements of " + "$A_" + str(d) + "$",
+                   linewidth=3)
+        plt.loglog([x for x in range(2, max_n)],
+                   [BlockMatrix(d, x).eval_zeros_lu()[0] for x in range(2, max_n)],
+                   label="nonzero elements of " + "$LU_" + str(d) + "$",
+                   linestyle='--', linewidth=3)
+
+    # finilize
+    plt.xlabel('N', fontsize=18)
+    plt.ylabel('number of nonzeros', fontsize=18)
+    plt.legend(fontsize=18)
+    plt.title("Plot of nonzero elements of A and LU depending on the dimension and size", fontsize=24)
+    plt.show()
+
+    a0 = [BlockMatrix(2, x).eval_zeros()[0] for x in range(3, max_n)]
+    lu = [BlockMatrix(2, x).eval_zeros_lu()[0] for x in range(3, max_n)]
+
+    # -----
+    plt.loglog([x for x in range(3, max_n)], [b - a for (a, b) in zip(a0, lu)],
+               label="nonzero elements of " + "$A_" + str(2) + "$",
+               linewidth=3)
+
+    plt.xlabel('N', fontsize=18)
+    plt.ylabel('number of nonzeros', fontsize=18)
+    plt.legend(fontsize=18)
+    plt.title("Difference of the nonzero LU and A", fontsize=24)
+    plt.show()
+
+def main():
+    """ Internal demo-function for testing (or demoing) this module. """
+    def demo_construction(d, n):
+        """
+        Demos block matrix for given dimension `d` and number of grid points `n` onto the terminal.
+
+        Parameters:
+        -----------
+        d : int
+            Dimension of the space
+        n : int
+            Number of intervals in each dimension
+        """
+        mat = BlockMatrix(d, n)
+        print("# Demo BlockMatrix(d={}, n={}) in R^{{{}x{}}}".format(d, n, mat.extend, mat.extend))
+        print("#======================================================")
+        # print(mat.data)
+        # print("#----------------------------------------------")
+        sp = mat.get_sparse()
+        print(sp.toarray())
+        non_zeros, zeros, rel_non_zeros, rel_zeros = mat.eval_zeros()
+        print("# non-zeros : {} (total number of non-zero values)".format(non_zeros))
+        print("# zeros     : {} (total number of zero values)".format(zeros))
+        print("% non-zeros : {:.2} (relative non-zero values)".format(rel_non_zeros))
+        print("% zeros     : {:.2} (relative zero values)".format(rel_zeros))
+        print()
+
+    def demo_lu(d, n):
+        """ Demonstration of the LU-decomposition.
+
+        Parameters
+        ----------
+        d : int
+            The dimension.
+        n : int
+            The number of grid points.
+        """
+        mat = BlockMatrix(d, n)
+        pr, l, u, pc = mat.get_lu()
+        print("Let d = {} and n = {}. For the LU-Decomposition, Pr * A * Pl = LU, we have:\n".format(d, n))
+        print("Row Permutation Matrix (Pr):\n{}\n".format(pr.toarray()))
+        print("The Matrix (A):\n{}\n".format(mat.data.toarray()))
+        print("Column Permutation Matrix (Pl):\n{}\n".format(pc.toarray()))
+        print("Lower Triangular Matrix (L):\n{}\n".format(l.toarray()))
+        print("Upper Triangular Matrix (U):\n{}\n".format(u.toarray()))
+
+    def demo_nnz(d, n):
+        """ Demonstration of the nonzero counting method..
+
+        Parameters
+        ----------
+        d : int
+            The dimension.
+        n : int
+            The number of grid points.
+        """
+        mat = BlockMatrix(d, n)
+        print("# NUMBER OF ZEROS LU DECOMPOSITION(d={}, n={}) in R^{{{}x{}}}".format(d, n, mat.extend, mat.extend))
+        print("#======================================================")
+        # demo_lu(d, n)
+        non_zeros, zeros, rel_non_zeros, rel_zeros = mat.eval_zeros_lu()
+        print("# non-zeros : {} (total number of non-zero values)".format(non_zeros))
+        print("# zeros     : {} (total number of zero values)".format(zeros))
+        print("% non-zeros : {:.2} (relative non-zero values)".format(float(rel_non_zeros)))
+        print("% zeros     : {:.2} (relative zero values)".format(float(rel_zeros)))
+        print()
+        print("Cond(A) = {}".format(mat.get_cond()))
+        print()
+
+    for d in [1, 2]:
+        for n in [2, 3, 4]:
+            demo_construction(d, n)
+            demo_nnz(d, n)
+
+    demo_lu(1, 5)
+    demo_lu(2, 4)
+
+    print("We can also draw plots.")
+    draw_cond()
+    draw_nonzero()
+
+if __name__ == "__main__":
+    main()
---
+++ fifth_element/linear_solvers.py
@@ -0,0 +1,133 @@
+#! /usr/bin/env python3
+"""
+"This module implements."
+-- linear solving.
+
+
+Author: Christian Parpart & Kei Thoma
+Date: 2019-11-13
+License: GPL-3
+"""
+import random
+
+import numpy as np
+from scipy import sparse as sm
+from scipy.sparse import linalg as slina
+
+def solve_lu(pr, l, u, pc, b):
+    """ Solves the linear system Ax = b via forward and backward substitution
+    given the decomposition pr * A * pc = l * u.
+
+    => A = pr^-1 * l * u * pc^-1
+
+    Parameters
+    ----------
+    pr : scipy.sparse.csr_matrix
+        row permutation matrix of LU-decomposition
+    l : scipy.sparse.csr_matrix
+        lower triangular unit diagonal matrix of LU-decomposition
+    u : scipy.sparse.csr_matrix
+        upper triangular matrix of LU-decomposition
+    pc : scipy.sparse.csr_matrix
+        column permutation matrix of LU-decomposition
+    b : numpy.ndarray
+        vector of the right-hand-side of the linear system
+
+    Returns
+    -------
+    x : numpy.ndarray
+        solution of the linear system
+    """
+    _ = slina.spsolve(sm.csc_matrix(slina.inv(sm.csc_matrix(pr))), b)
+    _ = slina.spsolve_triangular(l, _, lower=True)
+    _ = slina.spsolve_triangular(u, _, lower=False)
+    _ = slina.spsolve(sm.csc_matrix(slina.inv(sm.csc_matrix(pc))), _)
+    return _
+
+def solve_sor(A, b, x0, params=dict(eps=1e-8, max_iter=5, min_red=1e-4), omega=1.5):
+    """ Solves the linear system Ax = b via the successive over relaxation method.
+
+    Parameters
+    ----------
+    A : scipy.sparse.csr_matrix
+        system matrix of the linear system
+    b : numpy.ndarray
+        right-hand-side of the linear system
+    x0 : numpy.ndarray
+        initial guess of the solution
+    params : dict, optional
+        dictionary containing termination conditions
+        eps : float
+            tolerance for the norm of the residual in the infinity norm. If set
+            less or equal to 0 no constraint on the norm of the residual is imposed.
+        max_iter : int
+            maximal number of iterations that the solver will perform. If set
+            less or equal to 0 no constraint on the number of iterations is imposed.
+        min_red : float
+            minimal reduction of the residual in the infinity norm in every
+            step. If set less or equal to 0 no constraint on the norm of the
+            reduction of the residual is imposed.
+    omega : float, optional
+        relaxation parameter
+
+    Returns
+    -------
+    str
+        reason of termination. Key of the respective termination parameter.
+    list
+        iterates of the algorithm. First entry is ‘x0‘.
+    list
+        residuals of the iterates
+
+    Raises
+    ------
+    ValueError
+        If no termination condition is active, i.e., ‘eps=0‘ and ‘max_iter=0‘, etc.
+    """
+    def termination(x_k, it):
+        _ = A.multiply(x_k).todense()
+        _ = np.subtract(_, b)
+        residual = np.linalg.norm(_, np.inf)
+        if residual < params["eps"]:
+            return True
+        elif it > params["max_iter"]:
+            return True
+
+    def next_x(x_k, it=0):
+        # print("x_k = {}".format(x_k))
+        it += 1
+        if termination(x_k, it):
+            return x_k
+        else:
+            sol_x = []
+            for i in range(x0.size):
+                sum1 = sum([A[i, j] * sol_x[j] for j in range(i)])
+                sum2 = sum([A[i, j] * x_k[j] for j in range(i, x0.size)])
+                sol_x.append((1 - omega) * x_k[i] + (omega / A[i, i]) * (b[i] - sum1 - sum2))
+            return next_x(sol_x, it)
+    
+    return next_x(x0)
+
+def main():
+    """
+    JOIN THE GLORIOUS MAIN FUNCTION. Uses the test function to demonstrate.
+    """
+    mat = sm.csr_matrix(np.array([
+        [1, 0, 0, 0],
+        [0, 1, 0, 0],
+        [0, 0, 1, 0],
+        [0, 0, 0, 1]
+    ]))
+
+    mat2 = mat.todense()
+    # print(mat)
+    # print(mat[1, 2])
+
+    b = np.array([1, 2, 3, 4])
+
+    x0 = np.array([0, 0, 0, 0])
+
+    print(solve_sor(mat, b, x0))
+
+if __name__ == '__main__':
+    main()
