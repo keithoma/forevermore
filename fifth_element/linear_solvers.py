32e9196eda200e76a6d14f66a4886338006fbcf2
Christian Parpart <christian@parpart.family>
2020-03-15 02:00:18 +0100

    snapshot 2

--- fifth_element/linear_solvers.py
+++ fifth_element/linear_solvers.py
@@ -116,8 +116,6 @@ def solve_sor(A, b, x0, params=dict(eps=1e-8, max_iter=1000, min_red=1e-4), omeg
                 yield A[i, j] * x_k[j]
 
         for i in range(x_k.size):
-            #sum1 = sum([A[i, j] * sol_x[j] for j in range(i)])
-            #sum2 = sum([A[i, j] * x_k[j] for j in range(i + 1, x_k.size)])
             sum1 = sum(fsum1(i))
             sum2 = sum(fsum2(i + 1, x_k.size))
             sol_x.append((1 - omega) * x_k[i] + (omega / A[i, i]) * (b[i] - sum1 - sum2))
